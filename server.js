2623b8b87def247c8343cf8931a7816ad9e1ffe6
Ru-bot347 <russelnickpayod@gmail.com>
2024-12-02 09:57:22 +0800

    first commit

---
+++ server.js
@@ -0,0 +1,11 @@
+require("dotenv").config();
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+
+const taskRoutes = require("./routes/taskRoutes");
+app.use("/tasks", taskRoutes);
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
